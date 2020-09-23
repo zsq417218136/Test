2c1158d2275bf500f07844db846b2bd809e90365
zsq <417218136@qq.com>
2020-09-23 18:06:21 +0800

    2.添加rocketMQ

--- src/main/java/com/example/demo/test/test1.java
+++ src/main/java/com/example/demo/test/test1.java
@@ -1,19 +1,20 @@
 package com.example.demo.test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class test1 {
     public static void main(String[] args) {
-        List<String> a=new ArrayList<>();
-        a.add("gfgd");
-        a.add("g大大");
-        Vector c=new Vector();
-        c.add(1,"123");
-        c.add(2,a);
-        c.add(3,190797);
-        System.out.println(c.get(2));
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String ip2=ip.getHostAddress();
+            System.out.println("第二种方式"+ip2);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
     }
 }
